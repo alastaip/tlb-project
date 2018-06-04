b366e11f503f7f7318c2e5642438f261ebb8855d
Alastair <alastair.pearl@gmail.com>
2018-06-04 22:05:43 +1000

    Cleaned up controller code

--- TheLonelyBoy/Assets/Scripts/PlayerController.cs
+++ TheLonelyBoy/Assets/Scripts/PlayerController.cs
@@ -3,87 +3,107 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float moveSpeed;
-
-    public float jumpForce;
-
-    //private Rigidbody myRigidbody;
-
+    #region GameObjects
+    public GameObject playerModel;
     private CharacterController controller;
     private Vector3 moveDirection;
-    public float gravityScale;
-
     private Animator anim;
+    #endregion
 
+    #region MovementVars
+    public float fMoveSpeed;    //4
+    public float fJumpForce;    //20
+    public float fGravity;      //0.15
+    private float fTempYVelocity;
     public Transform pivot;
-    public float rotateSpeed;
+    public float fRotateSpeed;  //25
+    #endregion
 
-    public GameObject playerModel;
-   // public GameObject projectile;
-    //public Transform shootPoint;
-
-	// Use this for initialization
-	void Start () {
-
-        //myRigidbody = GetComponent<Rigidbody>();
+    void Start ()
+    {
         controller = GetComponent<CharacterController>();
         anim = GetComponentInChildren<Animator>();
-        
-
 	}
 	
-	// Update is called once per frame
 	void Update () {
         CheckDeath();
 
+        ResetState();
+        CheckState();
 
-        float yStore = moveDirection.y;
+        GetDirectionalInput();
+        CalculateVelocity();
+        GetActionInput();
 
-        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
-        moveDirection = moveDirection.normalized * moveSpeed;
-        moveDirection.y = yStore;
+        ApplyEnvironment();
 
-        if (controller.isGrounded)
-        {
-            moveDirection.y = 0f;
-            if (Input.GetButtonDown("Fire1"))
-            {
-                moveDirection.y = jumpForce;
-                
-            }
-        }
+        Move();
 
-
-        //Apply gravity
-        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
-
-
-        //Apply vector
-        controller.Move(moveDirection * Time.deltaTime);
-
-        //Move the player in different directions based on camera look direction
-        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") !=0)
-        {
-            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
-            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
-            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
-        }
-
-
-   
-        anim.SetBool("isGrounded", controller.isGrounded);
-        anim.SetFloat("moveSpeed", (Mathf.Abs(Input.GetAxis("Vertical")) + (Mathf.Abs(Input.GetAxis("Horizontal")))));
-
+        SetAnim();
     }
 
- 
     void CheckDeath()
     {
         if (controller.transform.position.y < -3)
         {
             controller.transform.position = new Vector3(7f, 1.1f, 6f);
             moveDirection = new Vector3(0, 0, 0);
-            
         }
+    }
+
+    void ResetState() { }
+
+    void CheckState() { }
+
+    void GetDirectionalInput()
+    {
+        fTempYVelocity = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+    }
+
+    void CalculateVelocity()
+    {
+        moveDirection = moveDirection.normalized * fMoveSpeed;
+        moveDirection.y = fTempYVelocity;   //?why
+    }
+
+    void GetActionInput()
+    {
+        #region Jump
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = fJumpForce;
+            }
+        }
+        #endregion
+    }
+
+    void ApplyEnvironment()
+    {
+        //Apply gravity
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * fGravity);
+    }
+
+    void Move()
+    {
+        //Apply vector
+        controller.Move(moveDirection * Time.deltaTime);
+
+        //Move the player in different directions based on camera look direction
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, fRotateSpeed * Time.deltaTime);
+        }
+    }
+
+    void SetAnim()
+    {
+        anim.SetBool("isGrounded", controller.isGrounded);
+        anim.SetFloat("moveSpeed", (Mathf.Abs(Input.GetAxis("Vertical")) + (Mathf.Abs(Input.GetAxis("Horizontal")))));
     }
 }
