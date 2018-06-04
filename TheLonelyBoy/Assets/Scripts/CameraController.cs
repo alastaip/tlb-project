17e53f2c7b103d8c58b9b13f952d7b830e30d953
michiruru <38682523+michiruru@users.noreply.github.com>
2018-06-04 19:26:52 +1000

    added 3d controller

---
+++ TheLonelyBoy/Assets/Scripts/CameraController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform target;
+
+    public Vector3 offset;
+
+    public bool useOffsetValue;
+
+    public float rotateSpeed;
+
+    public Transform pivot;
+
+    public float maxViewAngle;
+    public float minViewAngle;
+
+    public bool invertY;
+
+
+	// Use this for initialization
+	void Start () {
+        if (!useOffsetValue)
+        {
+            offset = target.position - transform.position;
+        }
+
+        pivot.transform.position = target.transform.position;
+        // pivot.transform.parent = target.transform;
+        pivot.transform.parent = null;
+
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        pivot.transform.position = target.transform.position;
+
+        /* //get x position of the mouse and rotate the target
+         float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+         pivot.Rotate(0, horizontal, 0);
+
+         //get Y position of the mouse and rotate the pivot
+         float vertical = Input.GetAxis("Mouse Y") * rotateSpeed; */
+
+        //get x position of right stick and rotate the target
+        float horizontal = Input.GetAxis("RightStickX") * rotateSpeed;
+        pivot.Rotate(0, horizontal, 0);
+
+        //get Y position of the rightstick and rotate the pivot
+        float vertical = Input.GetAxis("RightStickY") * rotateSpeed; 
+
+        if (invertY)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        }
+        else
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+
+        //limit up/down camera movement
+        if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0f, 0f);
+        }
+
+        if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0f, 0f);
+        }
+
+        //move the camera based on the current rotation of target and the original offset
+        float desiredYAngle = pivot.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //transform.position = target.position - offset;
+
+        if(transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y - 0.5f, transform.position.z);
+        }
+        transform.LookAt(target);	
+	}
+}
---
+++ TheLonelyBoy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float moveSpeed;
+
+    public float jumpForce;
+
+    //private Rigidbody myRigidbody;
+
+    private CharacterController controller;
+    private Vector3 moveDirection;
+    public float gravityScale;
+
+    private Animator anim;
+
+    public Transform pivot;
+    public float rotateSpeed;
+
+    public GameObject playerModel;
+   // public GameObject projectile;
+    //public Transform shootPoint;
+
+	// Use this for initialization
+	void Start () {
+
+        //myRigidbody = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        anim = GetComponentInChildren<Animator>();
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckDeath();
+
+
+        float yStore = moveDirection.y;
+
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
+
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Fire1"))
+            {
+                moveDirection.y = jumpForce;
+                
+            }
+        }
+
+
+        //Apply gravity
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+
+
+        //Apply vector
+        controller.Move(moveDirection * Time.deltaTime);
+
+        //Move the player in different directions based on camera look direction
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") !=0)
+        {
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+
+
+   
+        anim.SetBool("isGrounded", controller.isGrounded);
+        anim.SetFloat("moveSpeed", (Mathf.Abs(Input.GetAxis("Vertical")) + (Mathf.Abs(Input.GetAxis("Horizontal")))));
+
+    }
+
+ 
+    void CheckDeath()
+    {
+        if (controller.transform.position.y < -3)
+        {
+            controller.transform.position = new Vector3(7f, 1.1f, 6f);
+            moveDirection = new Vector3(0, 0, 0);
+            
+        }
+    }
+}
